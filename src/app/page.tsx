fa062cb7cc3f2478e56c0d8d05d6789ee69ad47e
DavidYang0429 <1443346890a@gmail.com>
2025-12-09 23:15:09 +0800

    更新首页内容1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { PhysicsHero } from "@/components/PhysicsHero";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-16 relative">
+    <div className="flex flex-col gap-16 relative overflow-hidden pb-20">
       {/* Hero Section */}
       <section className="flex flex-col justify-center gap-6 py-20 lg:py-32 relative z-10">
         <PhysicsHero />
